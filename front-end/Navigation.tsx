c4d3bbe8a0c11ba6796970d2afcff7b87c3d2606
Angshuman Halder <angshumanhalder@Angshumans-MacBook-Pro.local>
2021-07-27 21:38:30 +0530

    auto login if token is present

--- front-end/Navigation.tsx
+++ front-end/Navigation.tsx
@@ -6,17 +6,36 @@ import { Otp } from "./Screens/Otp";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StackParamsList } from "./types/navigationtypes";
 import { Chat } from "./Screens/Chat";
+import { useEffect } from "react";
+import { useState } from "react";
+import { getValueFor } from "./utils/secureStorage";
+import { TOKEN } from "./utils/constants";
 
 const Stack = createStackNavigator<StackParamsList>();
 
 export const Navigation: React.FC<{}> = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    (async () => {
+      if (await getValueFor(TOKEN)) {
+        setIsLoggedIn(true);
+      }
+    })();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="ChatAndUserFeed" component={ChatUserScreens} />
-        <Stack.Screen name="OTP" component={Otp} />
-        <Stack.Screen name="Chat" component={Chat} />
+        {isLoggedIn ? (
+          <>
+            <Stack.Screen name="ChatAndUserFeed" component={ChatUserScreens} />
+            <Stack.Screen name="Chat" component={Chat} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="OTP" component={Otp} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
